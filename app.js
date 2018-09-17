e02daefce7698ffdafdac3ca5959fc813055d928
harbin86 <harbin86@gmail.com>
2018-09-17 21:48:53 +0800

    Added event listener to dice roll button

--- app.js
+++ app.js
@@ -8,3 +8,29 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores = [0,0];
+var roundScore = 0;
+var activePlayer = 0;
+
+
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+//var x = document.querySelector('#score').textContent;
+
+var diceDOM = document.querySelector('.dice');
+diceDOM.style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+	var dice = Math.floor(Math.random() * 6) + 1;
+	diceDOM.style.display = 'block';
+	diceDOM.src = 'dice-' + dice + '.png';
+});
+
+
+
